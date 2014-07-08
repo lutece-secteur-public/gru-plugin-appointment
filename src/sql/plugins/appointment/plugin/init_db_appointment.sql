bef3290268ddb3eb40f8b6243cead9652a951209
vbroussard <vbroussard@users.noreply.github.com>
2014-07-08 15:11:32 +0000

    RENDEZVOUS-15 : Correction of CSS and updated SQL script.
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/applications/plugin-appointment@69537 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/appointment/plugin/init_db_appointment.sql
+++ src/sql/plugins/appointment/plugin/init_db_appointment.sql
@@ -1,2 +1,3 @@
 INSERT INTO appointment_calendar_template (id, title, description, template_path) VALUES (1,'Calendrier','Calendrier des créneaux disponibles et indisponibles','skin/plugins/appointment/calendar/appointment_form_calendar.html' );
 INSERT INTO appointment_calendar_template (id, title, description, template_path) VALUES (2,'Liste des créneaux disponibles','Liste des créneaux disponibles','skin/plugins/appointment/calendar/appointment_form_list_open_slots.html' );
+INSERT INTO appointment_calendar_template (id, title, description, template_path) VALUES (3,'Calendrier horizontal','Calendrier horizontal','skin/plugins/appointment/calendar/appointment_form_calendar_horizontal.html' );
