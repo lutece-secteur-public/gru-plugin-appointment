144a944685cd6f330d704b090c2b944b8d4d7ae2
EJO <elysa.jouve@gmail.com>
2016-01-25 17:49:31 +0100

    RENDEZVOUS-155: Added SQL upgrade for new "open days" calendar templates ; Deleted wrong "no week-end" calendar templates

---
+++ src/sql/plugins/appointment/upgrade/update_db_appointment_1.0.2-1.0.3.sql
@@ -0,0 +1,9 @@
+INSERT INTO appointment_calendar_template (id, title, description, template_path) VALUES (4,'Calendrier jours ouverts','Calendrier des créneaux disponibles et indisponibles (jours ouverts)','skin/plugins/appointment/calendar/appointment_form_calendar_opendays.html' );
+INSERT INTO appointment_calendar_template (id, title, description, template_path) VALUES (5,'Liste des créneaux disponibles jours ouverts','Liste des créneaux disponibles (jours ouverts)','skin/plugins/appointment/calendar/appointment_form_list_open_slots_opendays.html' );
+
+
+
+	
+
+
+
