f593e239ae4c74b120b949144830af1cb1afc8ae
abdelaziz ben jmaa <abdelaziz.benjmaa.external@atos.net>
2016-06-17 16:15:17 +0200

    RENDEZVOUS-176

--- src/sql/plugins/appointment/plugin/create_db_appointment.sql
+++ src/sql/plugins/appointment/plugin/create_db_appointment.sql
@@ -97,7 +97,7 @@ CREATE TABLE appointment_appointment (
 	id_action_cancel int NOT NULL,
 	id_admin_user int DEFAULT NULL,
 	has_notify INT DEFAULT 0,
-        nb_place_reserved INT DEFAULT 0,  
+        nb_place_reserved INT DEFAULT 1,  
         
 	PRIMARY KEY (id_appointment)
 );
--- src/sql/plugins/appointment/upgrade/update_db_appointment_1.0.2-1.0.3.sql
+++ src/sql/plugins/appointment/upgrade/update_db_appointment_1.0.2-1.0.3.sql
@@ -2,10 +2,8 @@ INSERT INTO appointment_calendar_template (id, title, description, template_path
 INSERT INTO appointment_calendar_template (id, title, description, template_path) VALUES (5,'Liste des créneaux disponibles jours ouverts','Liste des créneaux disponibles (jours ouverts)','skin/plugins/appointment/calendar/appointment_form_list_open_slots_opendays.html' );
 ALTER TABLE appointment_form ADD COLUMN date_limit DATE NULL ;
 ALTER TABLE appointment_form ADD COLUMN seizure_duration INT NOT NULL default '0';
-ALTER TABLE appointment_form ADD COLUMN maximum_number_of_booked_seats INT NOT NULL default '0';
+ALTER TABLE appointment_form ADD COLUMN maximum_number_of_booked_seats INT NOT NULL default '1';
 ALTER TABLE appointment_appointment ADD COLUMN nb_place_reserved INT  default '0';  
-
-	
-
+update appointment_form set maximum_number_of_booked_seats = 1 where maximum_number_of_booked_seats is null
 
 
