d64bc6a9c2dba638e16a41fca6f4ff253e186c76
abdelaziz ben jmaa <abdelaziz.benjmaa.external@atos.net>
2016-06-24 14:49:33 +0200

    RENDEZVOUS-157

--- src/java/fr/paris/lutece/plugins/appointment/web/AppointmentApp.java
+++ src/java/fr/paris/lutece/plugins/appointment/web/AppointmentApp.java
@@ -1175,7 +1175,7 @@ public class AppointmentApp extends MVCApplication
     public XPage getAppointmentFormSecondStep( HttpServletRequest request )
     {
         _appointmentFormService.removeValidatedAppointmentFromSession( request.getSession(  ) );
-
+        listAppointmentSlotDisponiblity.clear();
         String strIdForm = request.getParameter( PARAMETER_ID_FORM );
 
         if ( StringUtils.isNotEmpty( strIdForm ) && StringUtils.isNumeric( strIdForm ) )
