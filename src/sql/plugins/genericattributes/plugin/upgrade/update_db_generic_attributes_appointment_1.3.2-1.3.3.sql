85552585febd9e0dfe01d3107c574f3c1ac027b1
Guillaume Lépine <guillaume.lepine@sogeti.com>
2020-08-18 10:39:50 +0200

    RENDEZVOUS-406 : Date Entry field now identified by its code

---
+++ src/sql/plugins/genericattributes/plugin/upgrade/update_db_generic_attributes_appointment_1.3.2-1.3.3.sql
@@ -0,0 +1,8 @@
+ALTER TABLE genatt_entry CHANGE COLUMN is_shown_in_completeness used_in_correct_form_response SMALLINT DEFAULT '0';
+
+UPDATE genatt_field f SET f.CODE = 'default_date_value'
+WHERE f.id_entry IN  (
+SELECT e.id_entry FROM genatt_entry e
+INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+WHERE t.class_name = 'appointment.entryTypeDate'
+AND e.id_entry = f.id_entry);
