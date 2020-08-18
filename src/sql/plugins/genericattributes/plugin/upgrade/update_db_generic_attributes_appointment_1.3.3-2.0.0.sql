cff01d7185607cea34520a4649ca2cde699c112f
Guillaume Lépine <guillaume.lepine@sogeti.com>
2020-08-18 16:16:36 +0200

    RENDEZVOUS-446: height parameters now stored as its own field

--- src/sql/plugins/genericattributes/plugin/upgrade/update_db_generic_attributes_appointment_1.3.3-2.0.0.sql
+++ src/sql/plugins/genericattributes/plugin/upgrade/update_db_generic_attributes_appointment_1.3.3-2.0.0.sql
@@ -15,7 +15,12 @@ ALTER TABLE genatt_entry DROP COLUMN confirm_field_title
 INSERT INTO genatt_field ( id_entry, code, value)
 	SELECT id_entry, 'width', width from genatt_field WHERE width > 0 AND code not in  ('file_config', 'user_config');
 
+INSERT INTO genatt_field ( id_entry, code, value)
+	SELECT id_entry, 'height', height from genatt_field WHERE height > 0;
+	
 DELETE FROM genatt_field where code = 'file_config';
 DELETE FROM genatt_field where code = 'user_config';
 
 ALTER TABLE genatt_field DROP COLUMN width;
+	
+ALTER TABLE genatt_field DROP COLUMN height;
