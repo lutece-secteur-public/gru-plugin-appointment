7adaf8975459c1b073629668029748a5e838e838
Guillaume Lépine <guillaume.lepine@sogeti.com>
2020-08-18 10:42:54 +0200

    RENDEZVOUS-406 : Geoloc Entry field now identified by its code

--- src/sql/plugins/genericattributes/plugin/upgrade/update_db_generic_attributes_appointment_1.3.2-1.3.3.sql
+++ src/sql/plugins/genericattributes/plugin/upgrade/update_db_generic_attributes_appointment_1.3.2-1.3.3.sql
@@ -6,3 +6,17 @@ SELECT e.id_entry FROM genatt_entry e
 INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
 WHERE t.class_name = 'appointment.entryTypeDate'
 AND e.id_entry = f.id_entry);
+
+UPDATE genatt_field f SET f.CODE = f.title
+WHERE f.id_entry IN  (
+SELECT e.id_entry FROM genatt_entry e
+INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+WHERE t.class_name = 'appointment.entryTypeGeolocation'
+AND e.id_entry = f.id_entry);
+
+UPDATE genatt_field f SET f.title = null
+WHERE f.id_entry IN  (
+SELECT e.id_entry FROM genatt_entry e
+INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+WHERE t.class_name = 'appointment.entryTypeGeolocation'
+AND e.id_entry = f.id_entry);
