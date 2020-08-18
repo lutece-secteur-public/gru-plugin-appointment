6c9aab057830783eecf9e6767e9dc817dd183929
Guillaume Lépine <guillaume.lepine@sogeti.com>
2020-08-18 10:47:29 +0200

    RENDEZVOUS-406 : Files & Image Entry field now identified by its code

--- src/sql/plugins/genericattributes/plugin/upgrade/update_db_generic_attributes_appointment_1.3.2-1.3.3.sql
+++ src/sql/plugins/genericattributes/plugin/upgrade/update_db_generic_attributes_appointment_1.3.2-1.3.3.sql
@@ -41,3 +41,25 @@ SELECT e.id_entry FROM genatt_entry e
 INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
 WHERE t.class_name = 'appointment.entryTypeNumbering'
 AND e.id_entry = f.id_entry);
+
+UPDATE genatt_field f SET f.CODE = f.title
+WHERE f.id_entry IN  (
+SELECT e.id_entry FROM genatt_entry e
+INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+WHERE t.class_name IN( 'appointment.entryTypeImage', 'appointment.entryTypeFile')
+AND e.id_entry = f.id_entry);
+
+UPDATE genatt_field f SET f.CODE = 'file_config'
+WHERE f.id_entry IN  (
+SELECT e.id_entry FROM genatt_entry e
+INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+WHERE t.class_name IN( 'appointment.entryTypeImage', 'appointment.entryTypeFile')
+AND f.code is null
+AND e.id_entry = f.id_entry);
+
+UPDATE genatt_field f SET f.title = null
+WHERE f.id_entry IN  (
+SELECT e.id_entry FROM genatt_entry e
+INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+WHERE t.class_name IN( 'appointment.entryTypeImage', 'appointment.entryTypeFile')
+AND e.id_entry = f.id_entry);
