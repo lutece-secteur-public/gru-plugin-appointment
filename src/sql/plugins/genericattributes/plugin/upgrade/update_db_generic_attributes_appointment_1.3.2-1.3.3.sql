2ffd3f1e64ba0ec44f801f962de89baa9bdc5f19
Guillaume Lépine <guillaume.lepine@sogeti.com>
2020-08-18 10:43:45 +0200

    RENDEZVOUS-406 : Numbering Entry field now identified by its code

--- src/sql/plugins/genericattributes/plugin/upgrade/update_db_generic_attributes_appointment_1.3.2-1.3.3.sql
+++ src/sql/plugins/genericattributes/plugin/upgrade/update_db_generic_attributes_appointment_1.3.2-1.3.3.sql
@@ -20,3 +20,24 @@ SELECT e.id_entry FROM genatt_entry e
 INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
 WHERE t.class_name = 'appointment.entryTypeGeolocation'
 AND e.id_entry = f.id_entry);
+
+UPDATE genatt_field f SET f.VALUE = f.title
+WHERE f.id_entry IN  (
+SELECT e.id_entry FROM genatt_entry e
+INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+WHERE t.class_name = 'appointment.entryTypeNumbering'
+AND e.id_entry = f.id_entry);
+
+UPDATE genatt_field f SET f.CODE = 'prefix'
+WHERE f.id_entry IN  (
+SELECT e.id_entry FROM genatt_entry e
+INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+WHERE t.class_name = 'appointment.entryTypeNumbering'
+AND e.id_entry = f.id_entry);
+
+UPDATE genatt_field f SET f.title = null
+WHERE f.id_entry IN  (
+SELECT e.id_entry FROM genatt_entry e
+INNER JOIN genatt_entry_type t ON t.id_type = e.id_type
+WHERE t.class_name = 'appointment.entryTypeNumbering'
+AND e.id_entry = f.id_entry);
