9af81899a5aac0cae1c6d88f659a6e078d56361c
jrmartino <james.r.martino@gmail.com>
2020-10-27 09:44:44 -0400

    workflow related changes to allow us to run unit tests

--- src/java/fr/paris/lutece/plugins/appointment/service/FormService.java
+++ src/java/fr/paris/lutece/plugins/appointment/service/FormService.java
@@ -38,7 +38,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import org.apache.commons.collections.CollectionUtils;
 
