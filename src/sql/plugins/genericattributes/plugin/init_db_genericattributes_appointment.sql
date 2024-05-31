e2a9dea0b17d356069b9169f6330582298ed4fbb
atos-mohamed-b <126660650+atos-mohamed-b@users.noreply.github.com>
2024-05-31 17:33:43 +0200

    LUT-28045: Updated the initialisation of the Generic Attributes to avoid the use of hard values for the IDs

--- src/sql/plugins/genericattributes/plugin/init_db_genericattributes_appointment.sql
+++ src/sql/plugins/genericattributes/plugin/init_db_genericattributes_appointment.sql
@@ -1,32 +1,32 @@
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
-(123,'Bouton radio',0,0,0,'appointment.entryTypeRadioButton','dot-circle','appointment',1,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
-(124,'Case à cocher',0,0,0,'appointment.entryTypeCheckBox','check-square','appointment',2,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
-(125,'Commentaire',0,1,0,'appointment.entryTypeComment','comment','appointment',3,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
-(126,'Date',0,0,0,'appointment.entryTypeDate','calendar','appointment',4,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
-(127,'Liste déroulante',0,0,0,'appointment.entryTypeSelect','list-alt','appointment',5,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
-(128,'Zone de texte court',0,0,0,'appointment.entryTypeText','file-alt','appointment',6,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
-(129,'Zone de texte long',0,0,0,'appointment.entryTypeTextArea','sticky-note','appointment',7,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-(130,'Numérotation',0,0,0,'appointment.entryTypeNumbering','hashtag','appointment',8,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
-(131,'Regroupement',1,0,0,'appointment.entryTypeGroup','indent','appointment',9,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-(132,'Liste déroulante SQL',0,0,0,'appointment.entryTypeSelectSQL','list-alt','appointment',10,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-(133,'Géolocalisation',0,0,0,'appointment.entryTypeGeolocation','map-marked-alt','appointment',11,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-(134,'Session',0,0,0,'appointment.entryTypeSession','user','appointment',12,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-(135,'Utilisateur MyLutece',0,0,1,'appointment.entryTypeMyLuteceUser','user','appointment',13,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-(136,'Image',0,0,0,'appointment.entryTypeImage','image','appointment',14,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
-(137,'Fichier',0,0,0,'appointment.entryTypeFile','file','appointment',15,0);
-INSERT INTO genatt_entry_type (id_type,title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
-(138,'Numéro de téléphone',0,0,0,'appointment.entryTypePhone','phone-square','appointment',16,1);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
+('Bouton radio',0,0,0,'appointment.entryTypeRadioButton','dot-circle','appointment',1,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
+('Case à cocher',0,0,0,'appointment.entryTypeCheckBox','check-square','appointment',2,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
+('Commentaire',0,1,0,'appointment.entryTypeComment','comment','appointment',3,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
+('Date',0,0,0,'appointment.entryTypeDate','calendar','appointment',4,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
+('Liste déroulante',0,0,0,'appointment.entryTypeSelect','list-alt','appointment',5,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
+('Zone de texte court',0,0,0,'appointment.entryTypeText','file-alt','appointment',6,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
+('Zone de texte long',0,0,0,'appointment.entryTypeTextArea','sticky-note','appointment',7,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
+('Numérotation',0,0,0,'appointment.entryTypeNumbering','hashtag','appointment',8,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
+('Regroupement',1,0,0,'appointment.entryTypeGroup','indent','appointment',9,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
+('Liste déroulante SQL',0,0,0,'appointment.entryTypeSelectSQL','list-alt','appointment',10,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
+('Géolocalisation',0,0,0,'appointment.entryTypeGeolocation','map-marked-alt','appointment',11,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
+('Session',0,0,0,'appointment.entryTypeSession','user','appointment',12,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
+('Utilisateur MyLutece',0,0,1,'appointment.entryTypeMyLuteceUser','user','appointment',13,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
+('Image',0,0,0,'appointment.entryTypeImage','image','appointment',14,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES
+('Fichier',0,0,0,'appointment.entryTypeFile','file','appointment',15,0);
+INSERT INTO genatt_entry_type (title,is_group,is_comment,is_mylutece_user,class_name,icon_name,plugin,display_order,inactive) VALUES 
+('Numéro de téléphone',0,0,0,'appointment.entryTypePhone','phone-square','appointment',16,1);
