bf6f93f2bd42c38756dda346864c2abe90b317e7
Anas BENJELLOUN <anasbenjellountouimi@gmail.com>
2025-09-15 16:33:44 +0200

    LUT-30714 IndexOutOfBoundsException when processing appointments without associated slots

--- src/java/fr/paris/lutece/plugins/appointment/service/AppointmentService.java
+++ src/java/fr/paris/lutece/plugins/appointment/service/AppointmentService.java
@@ -465,8 +465,16 @@ public final class AppointmentService
     public static AppointmentDTO buildAppointmentDTOFromIdAppointment( int nIdAppointment )
     {
         Appointment appointment = AppointmentService.findAppointmentById( nIdAppointment );
+        if ( appointment == null )
+        {
+           return null;
+        }
         User user = UserService.findUserById( appointment.getIdUser( ) );
         List<Slot> listSlot = SlotService.findListSlotByIdAppointment( appointment.getIdAppointment( ) );
+        if ( CollectionUtils.isEmpty( listSlot ) )
+        {
+            return null;
+        }
         appointment.setSlot( listSlot );
         appointment.setUser( user );
         return buildAppointmentDTO( appointment );
