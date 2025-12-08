1a9d0db81bc699073be01174481336c2a2b92b32
Jeremy Deloche <jeremy.deloche@atos.com>
2025-12-08 09:41:19 +0100

    LUT-31298 : add APPOINTMENT_COMMENT_MANAGEMENT right in init

--- src/sql/plugins/appointment/core/init_core_appointment.sql
+++ src/sql/plugins/appointment/core/init_core_appointment.sql
@@ -9,6 +9,8 @@ INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is
 ('APPOINTMENT_FORM_MANAGEMENT','appointment.adminFeature.ManageAppointmentForm.name',1,'jsp/admin/plugins/appointment/ManageAppointmentForms.jsp','appointment.adminFeature.ManageAppointmentForm.description',0,'appointment','APPLICATIONS',NULL,NULL,4);
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
 ('APPOINTMENT_CALENDAR_TEMPLATE','appointment.adminFeature.manageCalendarTemplates.name',0,'jsp/admin/plugins/appointment/ManageCalendarTemplates.jsp','appointment.adminFeature.manageCalendarTemplates.name',0,'appointment','APPLICATIONS',NULL,NULL,5);
+INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES
+('APPOINTMENT_COMMENT_MANAGEMENT','appointment.adminFeature.manageComment.name',2,'jsp/admin/plugins/appointment/Comments.jsp','appointment.adminFeature.manageComment.name',0,'appointment','APPLICATIONS',NULL,NULL,6);
 
 
 INSERT INTO core_admin_role_resource (rbac_id,role_key,resource_type,resource_id,permission) VALUES
@@ -21,6 +23,8 @@ DELETE FROM core_user_right WHERE id_right = 'APPOINTMENT_FORM_MANAGEMENT';
 INSERT INTO core_user_right (id_right,id_user) VALUES ('APPOINTMENT_FORM_MANAGEMENT',1);
 DELETE FROM core_user_right WHERE id_right = 'APPOINTMENT_CALENDAR_TEMPLATE';
 INSERT INTO core_user_right (id_right,id_user) VALUES ('APPOINTMENT_CALENDAR_TEMPLATE',1);
+DELETE FROM core_user_right WHERE id_right = 'APPOINTMENT_COMMENT_MANAGEMENT';
+INSERT INTO core_user_right (id_right,id_user) VALUES ('APPOINTMENT_COMMENT_MANAGEMENT',1);
 
 
 INSERT INTO core_portlet_type VALUES ('APPOINTMENT_PORTLET','appointment.myAppointments.name','plugins/appointment/CreatePortletAppointment.jsp','plugins/appointment/ModifyPortletAppointment.jsp','fr.paris.lutece.plugins.appointment.business.portlet.AppointmentPortletHome','appointment','plugins/appointment/DoCreatePortletAppointment.jsp','/admin/portlet/script_create_portlet.html','/admin/plugins/appointment/portlet/create_portletappointment.html','','plugins/appointment/DoModifyPortletAppointment.jsp','/admin/portlet/script_modify_portlet.html','/admin/plugins/appointment/portlet/modify_portletappointment.html','',NULL);
